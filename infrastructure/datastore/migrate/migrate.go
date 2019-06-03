07ce9583900e9e4365b7cd17605dbd40d244d90e
nishikawatadashi <a151417k@st.u-gakugei.ac.jp>
2019-06-04 08:20:15 +0900

    リファクタ

--- infrastructure/datastore/migrate/migrate.go
+++ infrastructure/datastore/migrate/migrate.go
@@ -4,7 +4,7 @@ import "github.com/16francs/examin_go/infrastructure/datastore"
 
 func main() {
 	db := datastore.Connect()
-  defer db.Close()
+	defer db.Close()
 
 	// テーブルの作成
 	// db.AutoMifrate(&model.User{}) <- User テーブルの作成
