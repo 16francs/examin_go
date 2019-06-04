fb5390088ac59c097d9973a3aa57f845aa426389
wf-yamaday <e155417k@st.u-gakugei.ac.jp>
2019-06-04 19:09:34 +0900

    userモデルの作成
    userモデルの定義をした．
    それに合わせてマイグレーションファイルも用意した．
    制約のとこは緩めなのでリファクタリング候補

--- infrastructure/datastore/migrate/migrate.go
+++ infrastructure/datastore/migrate/migrate.go
@@ -1,6 +1,10 @@
 package main
 
-import "github.com/16francs/examin_go/infrastructure/datastore"
+import  (
+	"github.com/16francs/examin_go/infrastructure/datastore"
+
+	"github.com/16francs/examin_go/domain/model"
+)
 
 func main() {
 	db := datastore.Connect()
@@ -8,4 +12,6 @@ func main() {
 
 	// テーブルの作成
 	// db.AutoMifrate(&model.User{}) <- User テーブルの作成
-}
+
+	db.AutoMigrate(&model.User{})
+} 
