edef3e7f20b046fdaf8252fb19bc0c2b6721e041
Yamada Yuki <e155417k@st.u-gakugei.ac.jp>
2019-06-09 00:24:04 +0900

    コメントの追加
    jwtの設計についてコメントを追加した

--- interface/middleware/jwt.go
+++ interface/middleware/jwt.go
@@ -15,6 +15,14 @@ const (
 	iss    = "16france"
 )
 
+/*
+jwt tokenを生成する
+	iss tokenの発行者
+	sub tokenの利用者を一意に特定する識別子
+	iat tokenの発行時間
+	exp tokenの有効時間
+	role userの役割
+*/
 func GenerateToken(user model.User) (string, error) {
 
 	token := jwt.New(jwt.GetSigningMethod("HS256"))
@@ -30,7 +38,11 @@ func GenerateToken(user model.User) (string, error) {
 	return token.SignedString([]byte(secret))
 }
 
-func Parse(ctx *gin.Context) (string, error) {
+/*
+	jwt tokenから情報を取り出す
+	ここではlogin_idを返却する
+*/
+func Parse(ctx *gin.Context) (string, string, error) {
 
 	token, err := request.ParseFromRequest(ctx.Request, request.OAuth2Extractor,
 		func(token *jwt.Token) (interface{}, error) {
